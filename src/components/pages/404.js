4367945b82da278d8fde9efc72aa2c712154d060
Koliada Oleksii <koliada.oleksii@gmail.com>
2023-01-25 14:57:01 +0100

    React helmet v6

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,9 +1,15 @@
-import ErrorMessage from "../errorMessage/ErrorMessage"
+import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom"
+import ErrorMessage from "../errorMessage/ErrorMessage"
+
 
 const Page404 = () => {
     return (
         <div>
+            <Helmet>
+                <meta name="description" content="404 | Koliada.fr" />
+                <title>Oups.. | 404</title>
+            </Helmet>
             <ErrorMessage/>
             <Link style={{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px', 'marginTop': '30px'}} to="/">Back to main page</Link>
         </div>
--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,9 +1,14 @@
+import { Helmet } from "react-helmet";
 import ComicsList from "../comicsList/ComicsList";
 import AppBanner from "../appBanner/AppBanner";
 
 const ComicsPage = () => {
     return (
         <>
+            <Helmet>
+                <meta name="description" content="Page with list of our comics" />
+                <title>Comiks page | Koliada.fr</title>
+            </Helmet>
             <AppBanner/>
             <ComicsList/>
         </>
