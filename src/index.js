af46568cf10ae09884897c48b56a100273b4cc30
Bohdan Smachylo <smachyloooo@gmail.com>
2023-11-21 11:48:04 +0100

    Added the information of project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/app/App';
 
-import './style/style.scss';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
